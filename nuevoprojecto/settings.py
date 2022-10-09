481feee71ce9f3e524d681274feb7fd6c88681cd
Alex Rodriguez Villavicencio <83556616+AlexRodriguezVillavicencio@users.noreply.github.com>
2022-10-08 19:45:53 -0500

    Update settings.py

--- nuevoprojecto/settings.py
+++ nuevoprojecto/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!3g%1o*ga$m!^3r7mkb68l+^np_ff8c0*e4f784&0hhv4603js'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
